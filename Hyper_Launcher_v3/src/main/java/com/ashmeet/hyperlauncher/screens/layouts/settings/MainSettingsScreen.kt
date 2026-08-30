227e0440e3e72eca460470b4111621444f6497cb
ASHMEET <hollowlauncher@gmail.com>
2026-08-30 18:46:05 +0530

    clean up and fixes (part 2)

--- Hyper_Launcher_v3/src/main/java/com/ashmeet/hyperlauncher/screens/layouts/settings/MainSettingsScreen.kt
+++ Hyper_Launcher_v3/src/main/java/com/ashmeet/hyperlauncher/screens/layouts/settings/MainSettingsScreen.kt
@@ -18,6 +18,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.edit
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.ashmeet.hyperlauncher.screens.layouts.settings.layouts.CardPosition
@@ -125,7 +126,7 @@ fun MainSettingsScreen(
                     checked = forceEnglish,
                     onCheckedChange = {
                         forceEnglish = it
-                        LauncherPreferences.prefs.edit().putBoolean("force_english", it).apply()
+                        LauncherPreferences.prefs.edit { putBoolean("force_english", it) }
                         LauncherPreferences.loadPreferences(context)
                     }
                 )
